4175180e5a7ba400b62373d4a719b5f1218f00d1
Dominic Hopf <dmaphy@fedoraproject.org>
2011-03-13 15:25:52 +0100

    correct the page title

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ print '<?xml version="1.0"?>';
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 
 <head>
-	<title>Plugins for Geany</title>
+	<title>Geany Newsletters</title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="./stylesheets/mainstyle.css" />
 </head>
